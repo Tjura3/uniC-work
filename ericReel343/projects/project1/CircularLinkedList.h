dd2b62fde3582adee12d9996b67cde78777760c4
Thomas Juranich <tjuranich777@gmail.com>
2026-04-04 02:48:05 -0700

    a

--- ericReel343/projects/project1/CircularLinkedList.h
+++ ericReel343/projects/project1/CircularLinkedList.h
@@ -1,4 +1,7 @@
+#include <iostream>
+#include <initializer_list>
 #pragma once
+using namespace std;
 template <typename T>
 class CircularLinkedList {
     private:
@@ -7,5 +10,14 @@ class CircularLinkedList {
         Node* next;
         Node(T value) : data(val), next(nullptr){}
     };
+    Node* curr;
+    int size;
+
+    public:
+    CircularLinkedList() : curr(nullptr), size(0) {}
+    CircularLinkedList(initializer_list<T> inilist) : curr(nullptr), size(0){
+        
+    }
+    
     
 };
