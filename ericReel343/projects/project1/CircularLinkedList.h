8f1a858712023a201c31cf6bb6b8fc0922f622e9
Thomas Juranich <tjuranich777@gmail.com>
2026-04-03 21:30:15 -0700

    start

---
+++ ericReel343/projects/project1/CircularLinkedList.h
@@ -0,0 +1,11 @@
+#pragma once
+template <typename T>
+class CircularLinkedList {
+    private:
+    struct Node(){
+        T data;
+        Node* next;
+        Node(T value) : data(val), next(nullptr){}
+    };
+    
+};
