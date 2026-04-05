b0b0539ee34c8229b01257444b69722726069361
RAZORMIND\tjura <tjuranich777@gmail.com>
2026-04-05 01:59:35 -0700

    remember to finish project 1

--- ericReel343/projects/project1/CircularLinkedList.h
+++ ericReel343/projects/project1/CircularLinkedList.h
@@ -11,47 +11,104 @@ class CircularLinkedList {
         Node* next;
         Node(T val) : data(val), next(nullptr){}
     };
-    Node* curr;
-    int size;
+    Node* curr; //for current
+    int size_; //for some reason when it isn't an underscore, it conflicts with the name of the getter? 
+    //Looked it up, C++ does weird stuff when variables and functions are the same, so this one will be snake case? I can't find any class documentation on canvas at this time so that's what it will be for now.
 
     public:
-    CircularLinkedList() : curr(nullptr), size(0) {}
-    CircularLinkedList(initializer_list<T> inlist) : curr(nullptr), size(0){
-        for (const auto& listval : inlist) {
+    CircularLinkedList() : curr(nullptr), size_(0) {}
+    CircularLinkedList(const initializer_list<T>& inlist) : curr(nullptr), size_(0){
+        for (const auto& listval : inlist){
             add(listval);
         }
     }
+    ~CircularLinkedList(){
+        while(size_ != 0){
+            removeNext(1);
+        }
+    }
+
+    int size() const{
+        return size_;
+    }
+
+    T getCurrent() const{
+        return curr->data;
+    }
+
     void add(T val){
-        Node* newnode = new Node(val);
+        Node* newNode = new Node(val);
         if(curr == nullptr){
-            curr = newnode;
-            newnode->next = curr;
+            curr = newNode;
+            newNode->next = curr;
         }else{
             Node* temp = curr;
             while(temp->next != curr){
                 temp = temp->next;
 
             }
-            temp->next = newnode;
-            newnode->next = curr;
+            temp->next = newNode;
+            newNode->next = curr;
         }
-        size++;
+        size_++;
     }
     T removeNext(int place){ 
-        if(size == 0) return;
-        if(place == 0){
-            //unfortunately 
+        if(size_ == 0) return {};
+        if(place < 0){
+            cerr << "Error: Can't removeNext negative values. Updating input value to 0\n";
+            place = 0;
+        }
+
+        T returnVal;
+        //had a valgrind issue, not an issue moreso an error,
+        if (size_ == 1) {
+            returnVal = curr->data;
+            delete curr;
+            curr = nullptr;
+        }
+        else if(place == 0){
             Node* temp = curr;
             while(temp->next != curr){
                 temp = temp->next;
             }
             //at this point temp is the node before curr so set its next to node after curr, then delete curr, and update curr.
+            returnVal = curr->data;
             temp->next = curr->next;
             delete curr;
-            temp->next = curr;
+            curr = temp->next;
         }
+        else{
+            //specifically we want to stop at the point just before we do the thang
+            //also we are directly editing curr now, since we are in the circle.
+            for(int i = 1; i < place; i++){
+                curr = curr->next;
+            }
+            Node* toDel = curr->next;
+            returnVal = toDel->data;
+            curr->next = toDel->next;
+            delete toDel;
+            curr = curr->next;
+        }
+        size_--;
+        return returnVal;
     }
-    
-    
+
+    void visualize() const {
+        if (curr == nullptr) {
+            cout << "List is empty.\n";
+            return;
+        }
+
+        Node* temp = curr;
+        cout << "[Curr: " << curr->data << "]\n";
+        
+        //do whiles are built for this
+        do {
+            cout << "[" << temp->data << "]" << " -> ";
+            temp = temp->next;
+        } while (temp != curr);
+
+        cout << "[curr]\n";
+    }
     
 };
--- ericReel343/projects/project1/main.cpp
+++ ericReel343/projects/project1/main.cpp
@@ -1,16 +1,100 @@
 #include "CircularLinkedList.h"
 #include <iostream>
-using namespace std;
-int main(){
-    CircularLinkedList<string> residents({"Laing", "Wilder",  "Frobisher"});
 
+using namespace std;
+
+void runTests();
+
+
+int main(){
+    
+    //Testing the first scenario
+    CircularLinkedList<string> residents({"Laing", "Wilder",  "Frobisher"});
     while(residents.size() > 1) {
+        residents.visualize();
         string eaten = residents.removeNext(1);  // k == 1
         cout << "Eaten: " << eaten << "\n";
     }
-
     string winner = residents.getCurrent();
     cout << "The winner is " << winner << "!\n";
-    // cout << "worked"<< endl;
+
+
+    //One more test here to make sure the code runs right
+    CircularLinkedList<int> numbers({1, 2, 3, 4, 5});
+    while(numbers.size() > 1) {
+        numbers.visualize();
+        int killed = numbers.removeNext(3);  // k == 1
+        cout << "Killed: " << killed << "\n";
+    }
+    int success = numbers.getCurrent();
+    cout << "The winner is " << success << "!\n";
+
+    cout << "\n\nProper test time _________________________________________" << endl; // I know its proper to do endl but I feel like using \n at the end of a string is just more efficient, im not sure though.
+    //writing proper tests now
+    runTests();
+
+
     return 0;
+
+    
+
+}
+
+void runTests() {
+    int passed = 0;
+    int total = 0;
+
+    auto report = [&](bool condition, string name) {
+        total++;
+        if (condition) {
+            cout << "[PASS] " << name << endl;
+            passed++;
+        } else {
+            cout << "[FAIL] " << name << endl;
+        }
+    };
+
+    // --- TEST 1: The "Residents" Scenario ---
+    {
+        CircularLinkedList<string> residents({"Laing", "Wilder", "Frobisher"});
+        vector<string> eaten;
+        while(residents.size() > 1) {
+            eaten.push_back(residents.removeNext(1));
+        }
+        bool match = (eaten.size() == 2 && eaten[0] == "Wilder" && eaten[1] == "Laing");
+        bool winnerMatch = (residents.getCurrent() == "Frobisher");
+        report(match && winnerMatch, "Original Resident Scenario");
+    }
+
+    // --- TEST 2: Removing with k=0 (Current node) ---
+    {
+        CircularLinkedList<int> nums({10, 20, 30});
+        int removed = nums.removeNext(0); // Should remove 10 (curr)
+        report(removed == 10 && nums.getCurrent() == 20 && nums.size() == 2, "RemoveNext(0) Logic");
+    }
+
+    // --- TEST 3: Large k (Wrapping multiple times) ---
+    {
+        CircularLinkedList<string> chars({"A", "B", "C"});
+        // k=4: Move from A -> B -> C -> A -> B. Victim is B.
+        string victim = chars.removeNext(4);
+        report(victim == "B" && chars.getCurrent() == "C", "Large k Wrapping");
+    }
+
+    //goto empty
+    {
+        CircularLinkedList<int> singleton({99});
+        int val = singleton.removeNext(1); // Move 1 (back to 99), remove it.
+        report(val == 99 && singleton.size() == 0, "Go To Empy");
+    }
+
+    //Bounds check
+    {
+        CircularLinkedList<int> nums({1, 2});
+        int val = nums.removeNext(-5); 
+        report(val == 1 && nums.size() == 1, "If this doesn't say error above, it failed");
+    }
+
+    
+    cout << "Results: " << passed << " / " << total << " passed." << endl;
 }
